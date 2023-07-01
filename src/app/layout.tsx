77cf5710d2177e993bf8a1062988084007246d01
lonewolf1503 <104578544+lonewolf1503@users.noreply.github.com>
2023-07-01 16:42:04 +0530

    project initiated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import Navbar from '@/components/ui/Navbar'
+import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
-
+import {Inter} from "next/font/google"
 export const metadata = {
-  title: 'Breadit',
+  title: 'FeedIt',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
+
+const inter = Inter({subsets: ['latin']})
 
 export default function RootLayout({
   children,
@@ -11,8 +15,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  )
+    <div
+      lang="en"
+      className={cn('bg-white text-slate-900 antialiased', inter.className)}
+    >
+      <div>
+        <Navbar />
+        <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+          <div className="container max-w-7xl mx-auto pt-12 h-full">{children}</div>
+        </body>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+  <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+    <div className="container max-w-7xl mx-auto flex items-center justify-between gap-2">
+      {/* Logo */}
+      
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
